3281473542d7d8cf9a9307b13416807beaf60f10
thiago-you <thiago.youx@gmail.com>
2022-01-21 10:04:06 -0300

    creating initial sign up interface layout

--- app/src/main/java/you/thiago/cdp/ui/authentication/register/RegisterFragment.kt
+++ app/src/main/java/you/thiago/cdp/ui/authentication/register/RegisterFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -24,12 +23,7 @@ class RegisterFragment : Fragment() {
 
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
-        val root: View = binding.root
-        val textView: TextView = binding.textGallery
-
-        registerViewModel.text.observe(viewLifecycleOwner, { textView.text = it })
-
-        return root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
